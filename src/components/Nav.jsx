138a18a9f535822120dc5120f79077f9988ff8ce
Pedro Arantes <pedro.hga90@gmail.com>
2023-01-23 19:22:05 +0100

    react web app with routes

---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        Juan Renovation
+      </Link>
+      <ul>
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+      </ul>
+    </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
